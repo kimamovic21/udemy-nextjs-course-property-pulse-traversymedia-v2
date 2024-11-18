cd998a026280e1723a69ce6df79773987757c75f
kimamovic21 <imamovic.kerim@gmail.com>
2024-11-18 14:51:43 +0100

    13. Navbar Component Start

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,12 +1,4 @@
-// 'use client';
-// import { useRouter, useParams, useSearchParams, usePathname } from 'next/navigation';
-
 const PropertyPage = ({ params }) => {
-  // const router = useRouter();
-  // const { id } = useParams();
-  // const searchParams = useSearchParams();
-  // const pathname = usePathname();
-
   return (
     <div>PropertyPage {params.id}</div>
   );
