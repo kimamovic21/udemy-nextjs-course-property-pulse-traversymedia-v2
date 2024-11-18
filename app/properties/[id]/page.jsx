a8b320facc03b48cc06c64d38875049a92236156
kimamovic21 <imamovic.kerim@gmail.com>
2024-11-18 14:30:11 +0100

    12. Client Components & Navigation Hooks

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,6 +1,14 @@
-const PropertyPage = () => {
+// 'use client';
+// import { useRouter, useParams, useSearchParams, usePathname } from 'next/navigation';
+
+const PropertyPage = ({ params }) => {
+  // const router = useRouter();
+  // const { id } = useParams();
+  // const searchParams = useSearchParams();
+  // const pathname = usePathname();
+
   return (
-    <div>PropertyPage</div>
+    <div>PropertyPage {params.id}</div>
   );
 };
 
