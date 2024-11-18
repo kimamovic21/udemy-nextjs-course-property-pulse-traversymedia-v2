29bb6d945556ade97a94e01a09af795d2d6637c7
kimamovic21 <imamovic.kerim@gmail.com>
2024-11-18 13:38:11 +0100

    09. Metadata & Assets

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,11 @@
 import '@/assets/styles/globals.css';
 
+export const metadata = {
+  title: 'PropertyPulse | Find The Perfect Rental',
+  description: 'Find your dream rental property',
+  keywords: 'rental, find rentals, find properties',
+};
+
 const MainLayout = ({ children }) => {
   return (
     <html>
