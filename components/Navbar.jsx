c6bc7907de6eed2a3cc41719d42d005c6a3a5abc
kimamovic21 <imamovic.kerim@gmail.com>
2024-11-21 14:21:33 +0100

    31. Sign In Button

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,8 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { usePathname } from 'next/navigation';
+import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 import { FaGoogle } from 'react-icons/fa';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -9,11 +10,25 @@ import logo from '@/assets/images/logo-white.png';
 import profileDefault from '@/assets/images/profile.png';
 
 const Navbar = () => {
+  const { data: session } = useSession();
+  console.log('Session:', session);
+
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
   const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [providers, setProviders] = useState(null);
+  console.log('Providers: ', providers);
 
   const pathname = usePathname();
+
+  useEffect(() => {
+    const setAuthProviders = async () => {
+      const res = await getProviders();
+      console.log('Res: ', res);
+      setProviders(res);
+    };
+
+    setAuthProviders();
+  }, []);
 
   return (
     <nav className='bg-blue-700 border-b border-blue-500'>
@@ -73,7 +88,7 @@ const Navbar = () => {
                 >
                   Properties
                 </Link>
-                {isLoggedIn && (
+                {session && (
                   <Link
                     href='/properties/add'
                     className={`${pathname === '/properties/add' ? 'bg-black' : ''} text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2`}
@@ -86,19 +101,25 @@ const Navbar = () => {
           </div>
 
           {/* Right Side Menu (Logged Out) */}
-          {!isLoggedIn && (
+          {!session && (
             <div className='hidden md:block md:ml-6'>
               <div className='flex items-center'>
-                <button className='flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'>
-                  <i className='fa-brands fa-google text-white mr-2'></i>
-                  <span>Login or Register</span>
-                </button>
+                {providers && Object.values(providers).map((provider, index) => (
+                  <button 
+                    key={index} 
+                    className='flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
+                    onClick={() => signIn(provider.id)}
+                  >
+                    <FaGoogle className='text-white mr-2' />
+                    <span>Login or Register</span>
+                  </button>
+                ))}
               </div>
             </div>
           )}
 
           {/* Right Side Menu (Logged In) */}
-          {isLoggedIn && (
+          {session && (
             <div className='absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0'>
               <Link href='/messages' className='relative group'>
                 <button
@@ -211,7 +232,7 @@ const Navbar = () => {
             >
               Properties
             </Link>
-            {isLoggedIn && (
+            {session && (
               <Link
                 href='/properties/add'
                 className={`${pathname === '/properties/add' ? 'bg-black' : ''} text-white block rounded-md px-3 py-2 text-base font-medium`}
@@ -219,9 +240,9 @@ const Navbar = () => {
                 Add Property
               </Link>
             )}
-            {!isLoggedIn && (
+            {!session && (
               <button className='flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-4'>
-                 <FaGoogle className='text-white mr-2' />
+                <FaGoogle className='text-white mr-2' />
                 <span>Login or Register</span>
               </button>
             )}
