eb9dc5f6ff1c61294caadf7bc8a3481ff444938e
kimamovic21 <imamovic.kerim@gmail.com>
2024-11-27 17:00:13 +0100

    45. PropertyImages Component

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -4,6 +4,7 @@ import connectDB from '@/config/database';
 import Property from '@/models/Property';
 import PropertyHeaderImage from '@/components/PropertyHeaderImage';
 import PropertyDetails from '@/components/PropertyDetails';
+import PropertyImages from '@/components/PropertyImages';
 
 const PropertyPage = async ({ params }) => {
   await connectDB();
@@ -31,6 +32,8 @@ const PropertyPage = async ({ params }) => {
           </div>
         </div>
       </section>
+
+      <PropertyImages images={property.images} />
     </>
   );
 };
