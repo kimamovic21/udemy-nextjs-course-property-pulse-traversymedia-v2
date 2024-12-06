b1d00d28e2845924aab903c85645f480fb32f7c9
kimamovic21 <imamovic.kerim@gmail.com>
2024-12-06 14:59:30 +0100

    69. Mark As Read

--- components/MessageCard.jsx
+++ components/MessageCard.jsx
@@ -1,6 +1,26 @@
+'use client';
+
+import { useState } from 'react';
+import { toast } from 'react-toastify';
+import markMessageAsRead from '@/app/actions/markMessageAsRead';
+
 const MessageCard = ({ message }) => {
+  const [isRead, setIsRead] = useState(message.read);
+
+  const handleReadClick = async () => {
+    const read = await markMessageAsRead(message._id);
+    setIsRead(read);
+    toast.success(`Marked as ${read ? 'read' : 'new'}`);
+  };
+
   return (
     <div className='relative bg-white p-4 rounded-md shadow-md border border-gray-200'>
+      {!isRead && (
+        <div className='absolute top-2 right-2 bg-yellow-500 text-white px-2 py-1 rounded-md'>
+          New    
+        </div>
+      )}  
+
       <h2 className='text-xl mb-4'>
         <span className='font-bold mr-2'>Property Inquiry:</span>
         <span>{message.property.name}</span>
@@ -12,25 +32,28 @@ const MessageCard = ({ message }) => {
 
       <ul className='mt-4'>
         <li>
-          <strong>Reply Email:</strong>
+          <strong className='mr-1'>Reply Email:</strong>
           <a href={`mailto:${message.email}`} className='text-blue-500'>
             {message.email}
           </a>
         </li>
         <li>
-          <strong>Reply Phone:</strong>
+          <strong className='mr-1'>Reply Phone:</strong>
           <a href={`tel:${message.phone}`} className='text-blue-500'>
             {message.phone}
           </a>
         </li>
         <li>
-          <strong>Received:</strong>
-          {new Date(message.createdAt).toLocaleString()}
+          <strong className='mr-1'>Received:</strong>
+          <span>{new Date(message.createdAt).toLocaleString()}</span> 
         </li>
       </ul>
 
-      <button className='mt-4 mr-3 bg-blue-500 text-white py-1 px-3 rounded-md'>
-        Mark As Read
+      <button 
+        className='mt-4 mr-3 bg-blue-500 text-white py-1 px-3 rounded-md'
+        onClick={handleReadClick}
+      >
+        {isRead ? 'Mark As New' : 'Mark As Read'}
       </button>
 
       <button className='mt-4 bg-red-500 text-white py-1 px-3 rounded-md'>
